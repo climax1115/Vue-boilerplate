a3ac17d99b01effb611ffa4afc74fad29bb12dff
Kevin Edward <climax11150102.cmx@gmail.com>
2022-04-18 19:22:19 -0400

    first update

---
+++ src/views/Home/Jumbotron.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="jumbotron">
+    <div class="container text-center">
+      <img src="../../assets/awesome-vue.png" class="img-responsive">
+      <hello class="text-center"></hello>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from '@/components/Hello'
+
+export default {
+  name: 'jumbotron',
+  components: {
+    Hello
+  }
+}
+</script>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="view-home">
+  <jumbotron></jumbotron>
+</div>
+</template>
+
+<script>
+import Jumbotron from '@/views/Home/Jumbotron'
+
+export default {
+  components: {
+    Jumbotron
+  }
+}
+</script>
+
+<style>
+.view-home .jumbotron {
+  position: relative;
+  top:  -20px;
+}
+</style>
